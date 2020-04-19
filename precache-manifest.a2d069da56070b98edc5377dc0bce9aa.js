3038918913569f6733fc829df3c3068a9126db11
Kenzo <kenzo.ganbat@allycommerce.com>
2020-04-19 01:16:00 -0400

    Updates

---
+++ precache-manifest.a2d069da56070b98edc5377dc0bce9aa.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b3bc7f6c0900a2ba1aaaa97b1475c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "931a6607b3482f743f01",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "df1b392234313a6d0d90",
+    "url": "/static/css/main.c8f5a733.chunk.css"
+  },
+  {
+    "revision": "931a6607b3482f743f01",
+    "url": "/static/js/2.5e380108.chunk.js"
+  },
+  {
+    "revision": "59736d85d96f13d6eaaa5ba24857c523",
+    "url": "/static/js/2.5e380108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df1b392234313a6d0d90",
+    "url": "/static/js/main.3a39c448.chunk.js"
+  },
+  {
+    "revision": "53a65767f6862604811f",
+    "url": "/static/js/runtime-main.f01e5ca6.js"
+  },
+  {
+    "revision": "80c913c91c38d7d74a040876fdcac490",
+    "url": "/static/media/admag.80c913c9.jpg"
+  },
+  {
+    "revision": "cf4156a156342f599b0b6170f03a0be6",
+    "url": "/static/media/boldlu.cf4156a1.jpg"
+  },
+  {
+    "revision": "c2ecada59932fc260ecee7d8964a5324",
+    "url": "/static/media/devsor.c2ecada5.jpg"
+  },
+  {
+    "revision": "3f33d834736a7bf1c8d806eb17d67974",
+    "url": "/static/media/deyes.3f33d834.jpg"
+  },
+  {
+    "revision": "faef8fea462dfcdf62ac3207619ab724",
+    "url": "/static/media/embassy.faef8fea.jpg"
+  },
+  {
+    "revision": "7c37dacf774f843620e601b5029f90cd",
+    "url": "/static/media/fadmag.7c37dacf.jpg"
+  },
+  {
+    "revision": "579c578b56b75c086b07db75d3770488",
+    "url": "/static/media/fboldlu.579c578b.jpg"
+  },
+  {
+    "revision": "b19943e56412d466ebd2b05cc6eaf3bd",
+    "url": "/static/media/fdevsor.b19943e5.jpg"
+  },
+  {
+    "revision": "1e54e739f7f63509d4004e36293c630e",
+    "url": "/static/media/fdeyes.1e54e739.jpg"
+  },
+  {
+    "revision": "7c5df0dca79643891b245c543625eb2f",
+    "url": "/static/media/fembassy.7c5df0dc.jpg"
+  },
+  {
+    "revision": "0b3d60656fba87205b25e226bec86694",
+    "url": "/static/media/fkewiko.0b3d6065.jpg"
+  },
+  {
+    "revision": "710b74bcfd3af0d999467b90b8998dd8",
+    "url": "/static/media/fpolitik.710b74bc.jpg"
+  },
+  {
+    "revision": "5f967767c6a19edb923d0433c4a4e818",
+    "url": "/static/media/kewiko.5f967767.jpg"
+  },
+  {
+    "revision": "63f7a024e1e20f19e700108ba7b7fdc4",
+    "url": "/static/media/logo.63f7a024.jpg"
+  },
+  {
+    "revision": "7fc9be1a1872277c898d9121b1f07558",
+    "url": "/static/media/politik.7fc9be1a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ganzorig-ganbat/kenzo-portfolio/precache-manifest.09f52918689dee20a57e4c3b38cedcba.js"
+  "/precache-manifest.a2d069da56070b98edc5377dc0bce9aa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ganzorig-ganbat/kenzo-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
